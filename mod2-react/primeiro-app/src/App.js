79e22b2e533780764379a497594da730de69d4fb
Ariel Pellegrino <ariel.pell@gmail.com>
2025-02-24 21:48:10 -0300

    update: mod react

---
+++ mod2-react/primeiro-app/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Nome from "./components/Nome";
+
+function App() {
+  return (
+    <div>
+      <h1>Primeiro App</h1>
+      <Nome nome="João" />
+    </div>
+  );
+}
+
+export default App;
---
+++ mod2-react/primeiro-app/src/components/Nome.js
@@ -0,0 +1,9 @@
+
+
+export default function Nome(props){
+    return (
+        <div>
+            <h1>Meu nome é {props.nome}</h1>
+        </div>
+    )
+}
