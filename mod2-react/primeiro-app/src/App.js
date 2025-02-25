703230e960b816499d0dea6fcc8d0e885509a362
Ariel Pellegrino <ariel.pell@gmail.com>
2025-02-25 16:04:16 -0300

    update: mod react

--- mod2-react/primeiro-app/src/App.js
+++ mod2-react/primeiro-app/src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import Nome from "./components/Nome";
+import { useState } from "react";
 
 function App() {
+
+  const [aluno, setAluno] = useState('Sujeito PROGRAMADOR');
+
   return (
     <div>
       <h1>Primeiro App</h1>
-      <Nome nome="João" />
+          <Nome nome="João" idade={30} />
     </div>
   );
 }
--- mod2-react/primeiro-app/src/components/Nome.js
+++ mod2-react/primeiro-app/src/components/Nome.js
@@ -3,7 +3,7 @@
 export default function Nome(props){
     return (
         <div>
-            <h1>Meu nome é {props.nome}</h1>
+            <h1>Meu nome é {props.nome} e tenho {props.idade} anos</h1>
         </div>
     )
 }
